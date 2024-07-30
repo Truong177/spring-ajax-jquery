0965282f5a16d9ef69defd2e174b831cdb575f5f
Truong177 <truongtranquang773@gmail.com>
2024-07-30 09:06:55 +0700

    bai tap

---
+++ src/main/java/com/example/springajaxjquery/SpringAjaxJqueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springajaxjquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAjaxJqueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAjaxJqueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springajaxjquery/SpringAjaxJqueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springajaxjquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAjaxJqueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
